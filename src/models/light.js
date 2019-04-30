d3b5c8eaf6900c0fb48ab8d9a3f8235cc7331e7c
Nick Rook <rooknj@outlook.com>
2019-04-30 18:37:03 -0400

    added a light model

---
+++ src/models/light.js
@@ -0,0 +1,31 @@
+// http://docs.sequelizejs.com/variable/index.html#static-variable-DataTypes
+// TODO: Check out validations: http://docs.sequelizejs.com/manual/models-definition.html#validations
+module.exports = (sequelize, type) => {
+  return sequelize.define("light", {
+    id: {
+      type: type.STRING,
+      primaryKey: true
+    },
+    name: type.STRING,
+    supportedEffects: type.TEXT, // TEXT is unlimited length string
+    ipAddress: {
+      type: type.STRING,
+      validate: {
+        isIPv4: true
+      }
+    },
+    macAddress: {
+      type: type.STRING,
+      validate: {
+        is: ["^([0-9a-fA-F][0-9a-fA-F]:){5}([0-9a-fA-F][0-9a-fA-F])$"]
+      }
+    },
+    numLeds: { type: type.INTEGER },
+    udpPort: type.INTEGER,
+    version: type.STRING,
+    hardware: type.STRING,
+    colorOrder: type.STRING,
+    stripType: type.STRING,
+    rank: type.INTEGER
+  });
+};
