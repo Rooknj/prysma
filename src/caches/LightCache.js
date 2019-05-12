6b1ccaf9a8dd08a52739eb32bc7272e06f9f7b2a
Nick Rook <Nick_Rook@intuit.com>
2019-05-12 11:06:59 -0700

    added LightCache outline

---
+++ src/caches/LightCache.js
@@ -0,0 +1,11 @@
+class LightCache {
+  constructor() {
+    this._lightStates = [];
+  }
+
+  getLightState(lightId, lightState) {}
+
+  setLightState(lightId, lightState) {}
+}
+
+module.exports = LightCache;
