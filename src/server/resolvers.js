c0252e173066e8ef99abcd370153ee7975be6b26
Nick Rook <Nick_Rook@intuit.com>
2019-05-19 14:28:55 -0700

    added lightService resolvers

--- src/server/resolvers.js
+++ src/server/resolvers.js
@@ -1,3 +1,41 @@
 "use strict";
+const resolvers = {
+  Query: {
+    light: (_, { lightId }, { lightService }) => lightService.getLight(lightId),
+    lights: (_, args, { lightService }) => lightService.getLights(),
+    discoveredLights: (_, args, { lightService }) =>
+      lightService.getDiscoveredLights()
+  },
+  Mutation: {
+    setLight: (_, { lightId, lightData }, { lightService }) =>
+      lightService.setLight(lightId, lightData),
+    addLight: (_, { lightId, lightName }, { lightService }) =>
+      lightService.addLight(lightId, lightName),
+    removeLight: (_, { lightId }, { lightService }) =>
+      lightService.removeLight(lightId)
+  }
+  // Subscription: {
+  //   lightChanged: {
+  //     subscribe: (_, { lightId }, { subscriptionService }) => {
+  //       return subscriptionService.subscribeToLight(lightId);
+  //     }
+  //   },
+  //   lightsChanged: {
+  //     subscribe: (_, args, { subscriptionService }) => {
+  //       return subscriptionService.subscribeToAllLights();
+  //     }
+  //   },
+  //   lightAdded: {
+  //     subscribe: (_, args, { subscriptionService }) => {
+  //       return subscriptionService.subscribeToLightsAdded();
+  //     }
+  //   },
+  //   lightRemoved: {
+  //     subscribe: (_, args, { subscriptionService }) => {
+  //       return subscriptionService.subscribeToLightsRemoved();
+  //     }
+  //   }
+  // }
+};
 
-module.exports = {};
+module.exports = resolvers;
