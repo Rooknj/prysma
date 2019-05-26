9abbe0bdbf6a66ffbb4c59d06d46aa3fdb053aad
Nick Rook <Nick_Rook@intuit.com>
2019-05-25 23:35:08 -0700

    wrote lightUtil tests

--- src/utils/lightUtils.js
+++ src/utils/lightUtils.js
@@ -1,14 +1,14 @@
 module.exports = {
-  toLightObject: light => {
-    const plainLight = Object.assign({}, light.get({ plain: true }));
+  toLightObject: lightModel => {
+    const plainLight = Object.assign({}, lightModel);
     plainLight.supportedEffects =
       plainLight.supportedEffects.length > 0
         ? plainLight.supportedEffects.split(",")
         : [];
     return plainLight;
   },
-  toLightModel: light => {
-    const lightModel = Object.assign({}, light);
+  toLightModel: lightObject => {
+    const lightModel = Object.assign({}, lightObject);
     if ("supportedEffects" in lightModel)
       lightModel.supportedEffects = lightModel.supportedEffects.join(",");
     return lightModel;
---
+++ src/utils/lightUtils.test.js
@@ -0,0 +1,55 @@
+const lightUtils = require("./lightUtils");
+
+describe("toLightObject", () => {
+  test("correctly converts supportedEffects", () => {
+    const supportedEffects = "test 1,mock effect,cylon,juggle";
+    const expectedEffects = ["test 1", "mock effect", "cylon", "juggle"];
+    const LIGHT_MODEL = { supportedEffects };
+
+    const lightObject = lightUtils.toLightObject(LIGHT_MODEL);
+
+    expect(lightObject.supportedEffects).toEqual(expectedEffects);
+  });
+  test("converts empty supported effects to an empty array", () => {
+    const supportedEffects = "";
+    const expectedEffects = [];
+    const LIGHT_MODEL = { supportedEffects };
+
+    const lightObject = lightUtils.toLightObject(LIGHT_MODEL);
+
+    expect(lightObject.supportedEffects).toEqual(expectedEffects);
+  });
+});
+describe("toLightModel", () => {
+  test("correctly converts supportedEffects to a string", () => {
+    const supportedEffects = ["test 1", "mock effect", "cylon", "juggle"];
+    const expectedEffects = "test 1,mock effect,cylon,juggle";
+    const LIGHT_OBJECT = { supportedEffects };
+
+    const lightModel = lightUtils.toLightModel(LIGHT_OBJECT);
+
+    expect(lightModel.supportedEffects).toBe(expectedEffects);
+  });
+  test("converts empty supported effects to an empty string", () => {
+    const supportedEffects = [];
+    const expectedEffects = "";
+    const LIGHT_OBJECT = { supportedEffects };
+
+    const lightModel = lightUtils.toLightModel(LIGHT_OBJECT);
+
+    expect(lightModel.supportedEffects).toBe(expectedEffects);
+  });
+});
+
+describe("getSimpleUniqueId", () => {
+  test("Returns a number", () => {
+    const uniqueId = lightUtils.getSimpleUniqueId();
+
+    expect(typeof uniqueId).toBe("number");
+  });
+  test("Returns an integer", () => {
+    const uniqueId = lightUtils.getSimpleUniqueId();
+
+    expect(parseInt(uniqueId)).toBe(uniqueId);
+  });
+});
