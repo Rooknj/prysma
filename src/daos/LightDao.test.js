b59f50bda3f019c0f48cd510c86f463abdbc3d1e
Nick Rook <Nick_Rook@intuit.com>
2019-05-25 22:20:07 -0700

    fixed lightDao tests

--- src/daos/LightDao.test.js
+++ src/daos/LightDao.test.js
@@ -1,4 +1,4 @@
-const PrysmaDB = require("./LightDao");
+const LightDao = require("./LightDao");
 const { toLightModel } = require("../utils/lightUtils");
 
 jest.mock("../clients/db", () => {
@@ -30,11 +30,13 @@ jest.mock("../clients/db", () => {
   return { getDb };
 });
 
+let lightDao;
 let mockLightModel;
 const NO_ID_ERROR = "No ID provided";
 const NO_DATA_ERROR = "No Data provided";
 
 beforeEach(() => {
+  lightDao = new LightDao();
   mockLightModel = {
     id: "Mock1",
     name: "Mock Light",
@@ -53,18 +55,14 @@ beforeEach(() => {
 
 describe("constructor", () => {
   test("initializes correctly", () => {
-    const prysmaDb = new PrysmaDB();
-
-    expect(prysmaDb._db).not.toBeNull();
+    expect(lightDao._db).not.toBeNull();
   });
 });
 
 describe("getLight", () => {
   test("Returns the light with the specified id (1)", async () => {
-    const prysmaDb = new PrysmaDB();
-
     const ID = "Mock 1";
-    const testLight = await prysmaDb.getLight(ID);
+    const testLight = await lightDao.getLight(ID);
 
     expect(testLight.id).toBe(ID);
     expect(Object.keys(testLight)).toEqual(
@@ -72,10 +70,8 @@ describe("getLight", () => {
     );
   });
   test("Returns the light with the specified id (2)", async () => {
-    const prysmaDb = new PrysmaDB();
-
     const ID = "Prysma-AABBCCDD1144";
-    const testLight = await prysmaDb.getLight(ID);
+    const testLight = await lightDao.getLight(ID);
 
     expect(testLight.id).toBe(ID);
     expect(Object.keys(testLight)).toEqual(
@@ -83,51 +79,34 @@ describe("getLight", () => {
     );
   });
   test("Rejects if no id is given", async () => {
-    const prysmaDb = new PrysmaDB();
+    const dbPromise = lightDao.getLight();
 
-    try {
-      await prysmaDb.getLight();
-    } catch (error) {
-      expect(error).toBeInstanceOf(Error);
-      expect(error.message).toBe(NO_ID_ERROR);
-    }
+    expect(dbPromise).rejects.toThrow(NO_ID_ERROR);
   });
   test("Rejects if the light wasnt found", async () => {
-    const prysmaDb = new PrysmaDB();
-
-    prysmaDb._db.findByPk = jest.fn();
+    lightDao._db.findByPk = jest.fn();
     const ID = "Prysma-112233445566";
 
-    try {
-      await prysmaDb.getLight(ID);
-    } catch (error) {
-      expect(error).toBeInstanceOf(Error);
-      expect(error.message).toBe(`"${ID}" not found`);
-    }
+    const dbPromise = lightDao.getLight(ID);
+
+    expect(dbPromise).rejects.toThrow(`"${ID}" not found`);
   });
   test("Rejects if it cant get the light", async () => {
-    const prysmaDb = new PrysmaDB();
-
     const ERROR_MESSAGE = "Mock Error";
-    prysmaDb._db.findByPk = jest.fn(async () => {
+    lightDao._db.findByPk = jest.fn(async () => {
       throw new Error(ERROR_MESSAGE);
     });
     const ID = "Prysma-112233445566";
 
-    try {
-      await prysmaDb.getLight(ID);
-    } catch (error) {
-      expect(error).toBeInstanceOf(Error);
-      expect(error.message).toBe(ERROR_MESSAGE);
-    }
+    const dbPromise = lightDao.getLight(ID);
+
+    expect(dbPromise).rejects.toThrow(ERROR_MESSAGE);
   });
 });
 
 describe("getLights", () => {
   test("Returns all the lights", async () => {
-    const prysmaDb = new PrysmaDB();
-
-    const testLights = await prysmaDb.getLights();
+    const testLights = await lightDao.getLights();
 
     expect(Array.isArray(testLights));
     expect(Object.keys(testLights[0])).toEqual(
@@ -135,26 +114,19 @@ describe("getLights", () => {
     );
   });
   test("Rejects if it cant get the lights", async () => {
-    const prysmaDb = new PrysmaDB();
-
     const ERROR_MESSAGE = "Mock Error";
-    prysmaDb._db.findAll = jest.fn(async () => {
+    lightDao._db.findAll = jest.fn(async () => {
       throw new Error(ERROR_MESSAGE);
     });
 
-    try {
-      await prysmaDb.getLights();
-    } catch (error) {
-      expect(error).toBeInstanceOf(Error);
-      expect(error.message).toBe(ERROR_MESSAGE);
-    }
+    const dbPromise = lightDao.getLights();
+
+    expect(dbPromise).rejects.toThrow(ERROR_MESSAGE);
   });
 });
 
 describe("setLight", () => {
   test("Correctly sets the light", async () => {
-    const prysmaDb = new PrysmaDB();
-
     const ID = "Mock 1";
     const DATA = {
       name: "New name",
@@ -170,40 +142,28 @@ describe("setLight", () => {
       rank: 2
     };
     mockLightModel.update = jest.fn();
-    prysmaDb._db.findByPk = jest.fn(() => mockLightModel);
-    await prysmaDb.setLight(ID, DATA);
+    lightDao._db.findByPk = jest.fn(() => mockLightModel);
+    await lightDao.setLight(ID, DATA);
 
-    expect(prysmaDb._db.findByPk).toBeCalledTimes(1);
-    expect(prysmaDb._db.findByPk).toBeCalledWith(ID);
+    expect(lightDao._db.findByPk).toBeCalledTimes(1);
+    expect(lightDao._db.findByPk).toBeCalledWith(ID);
     expect(mockLightModel.update).toBeCalledTimes(1);
     expect(mockLightModel.update).toBeCalledWith(
       expect.objectContaining(toLightModel(DATA))
     );
   });
   test("Rejects if no id is given", async () => {
-    const prysmaDb = new PrysmaDB();
+    const dbPromise = lightDao.setLight();
 
-    try {
-      await prysmaDb.setLight();
-    } catch (error) {
-      expect(error).toBeInstanceOf(Error);
-      expect(error.message).toBe(NO_ID_ERROR);
-    }
+    expect(dbPromise).rejects.toThrow(NO_ID_ERROR);
   });
   test("Rejects if no data is given", async () => {
-    const prysmaDb = new PrysmaDB();
+    const dbPromise = lightDao.setLight("Mock ID");
 
-    try {
-      await prysmaDb.setLight("Mock ID");
-    } catch (error) {
-      expect(error).toBeInstanceOf(Error);
-      expect(error.message).toBe(NO_DATA_ERROR);
-    }
+    expect(dbPromise).rejects.toThrow(NO_DATA_ERROR);
   });
   test("Rejects if the light doesnt exist", async () => {
-    const prysmaDb = new PrysmaDB();
-
-    prysmaDb._db.findByPk = jest.fn();
+    lightDao._db.findByPk = jest.fn();
     const ID = "Prysma-Mock";
     const DATA = {
       name: "New name",
@@ -219,16 +179,11 @@ describe("setLight", () => {
       rank: 2
     };
 
-    try {
-      await prysmaDb.setLight(ID, DATA);
-    } catch (error) {
-      expect(error).toBeInstanceOf(Error);
-      expect(error.message).toBe(`"${ID}" not found`);
-    }
+    const dbPromise = lightDao.setLight(ID, DATA);
+
+    expect(dbPromise).rejects.toThrow(`"${ID}" not found`);
   });
   test("Rejects if it cant set the light", async () => {
-    const prysmaDb = new PrysmaDB();
-
     const ID = "Mock 1";
     const DATA = {
       name: "New name",
@@ -247,24 +202,19 @@ describe("setLight", () => {
     mockLightModel.update = jest.fn(async () => {
       throw new Error(ERROR_MESSAGE);
     });
-    prysmaDb._db.findByPk = jest.fn(() => mockLightModel);
+    lightDao._db.findByPk = jest.fn(() => mockLightModel);
 
-    try {
-      await prysmaDb.setLight(ID, DATA);
-    } catch (error) {
-      expect(error).toBeInstanceOf(Error);
-      expect(error.message).toBe(ERROR_MESSAGE);
-    }
+    const dbPromise = lightDao.setLight(ID, DATA);
+
+    expect(dbPromise).rejects.toThrow(ERROR_MESSAGE);
   });
 });
 
 describe("addLight", () => {
   test("Adds the light", async () => {
-    const prysmaDb = new PrysmaDB();
-
     const ID = "Prysma-334455667788";
     const NAME = "Window";
-    const testLight = await prysmaDb.addLight(ID, NAME);
+    const testLight = await lightDao.addLight(ID, NAME);
 
     expect(testLight.id).toBe(ID);
     expect(testLight.name).toBe(NAME);
@@ -273,10 +223,8 @@ describe("addLight", () => {
     );
   });
   test("Adds the light with name = ID if no name was given", async () => {
-    const prysmaDb = new PrysmaDB();
-
     const ID = "Prysma-334455667788";
-    const testLight = await prysmaDb.addLight(ID);
+    const testLight = await lightDao.addLight(ID);
 
     expect(testLight.id).toBe(ID);
     expect(testLight.name).toBe(ID);
@@ -285,26 +233,22 @@ describe("addLight", () => {
     );
   });
   test("Rejects if no id is given", async () => {
-    const prysmaDb = new PrysmaDB();
-
     try {
-      await prysmaDb.addLight();
+      await lightDao.addLight();
     } catch (error) {
       expect(error).toBeInstanceOf(Error);
       expect(error.message).toBe(NO_ID_ERROR);
     }
   });
   test("Rejects if it cant add the light", async () => {
-    const prysmaDb = new PrysmaDB();
-
     const ERROR_MESSAGE = "Mock Error Add";
-    prysmaDb._db.create = jest.fn(async () => {
+    lightDao._db.create = jest.fn(async () => {
       throw new Error(ERROR_MESSAGE);
     });
     const ID = "Prysma-112233445566";
 
     try {
-      await prysmaDb.addLight(ID);
+      await lightDao.addLight(ID);
     } catch (error) {
       expect(error).toBeInstanceOf(Error);
       expect(error.message).toBe(ERROR_MESSAGE);
@@ -314,35 +258,23 @@ describe("addLight", () => {
 
 describe("removeLight", () => {
   test("Removes the light", async () => {
-    const prysmaDb = new PrysmaDB();
-
     const ID = "Prysma-334455667788";
     const NAME = "Mock Light";
-    const testLight = await prysmaDb.removeLight(ID);
+    const testLight = await lightDao.removeLight(ID);
 
     expect(testLight).toEqual({ id: ID, name: NAME });
   });
   test("Rejects if the light doesnt exist", async () => {
-    const prysmaDb = new PrysmaDB();
-
-    prysmaDb._db.findByPk = jest.fn();
+    lightDao._db.findByPk = jest.fn();
     const ID = "Prysma-334455667788";
 
-    try {
-      await prysmaDb.removeLight(ID);
-    } catch (error) {
-      expect(error).toBeInstanceOf(Error);
-      expect(error.message).toBe(`"${ID}" not found`);
-    }
+    const dbPromise = lightDao.removeLight(ID);
+
+    expect(dbPromise).rejects.toThrow(`"${ID}" not found`);
   });
   test("Rejects if no id is given", async () => {
-    const prysmaDb = new PrysmaDB();
+    const dbPromise = lightDao.removeLight();
 
-    try {
-      await prysmaDb.removeLight();
-    } catch (error) {
-      expect(error).toBeInstanceOf(Error);
-      expect(error.message).toBe(NO_ID_ERROR);
-    }
+    expect(dbPromise).rejects.toThrow(NO_ID_ERROR);
   });
 });
