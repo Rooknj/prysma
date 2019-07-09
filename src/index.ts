d913118a2d4cc40a3f0ae84f89b2c41f1566c192
Nick Rook <Nick_Rook@intuit.com>
2019-07-08 22:26:37 -0700

    fixed build error

--- src/index.ts
+++ src/index.ts
@@ -55,7 +55,8 @@ process.on(
     resolvers: [LightResolver],
     // Automatically create `schema.gql` file with schema definition in current folder if not running from a pkg executable
     // Don't create the schema file if running from a pkg executable
-    emitSchemaFile: process.env.NODE_ENV ? path.resolve(__dirname, "schema.gql") : false,
+    emitSchemaFile:
+      process.env.NODE_ENV === "development" ? path.resolve(__dirname, "schema.gql") : false,
     // Use our custom PubSub system
     pubSub,
   });
